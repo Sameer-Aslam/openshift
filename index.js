4aea1cae2f9f95455e698efaca135933e04f4b94
Sameer Aslam <sameeraslam660@gmail.com>
2025-10-08 15:25:21 +0500

    update

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = process.env.PORT || 8080;
 
 app.get("/", (req, res) => {
-  res.send("🚀 Hello from My New OpenShift App!");
+  res.send("🚀 Hello from My New OpenShift App! its also updated to new version (v1.2).");
 });
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
